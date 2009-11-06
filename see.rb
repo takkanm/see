4c1d626b938afd4e21f817db6f70258427715256
Keita Urashima <ursm@ursm.jp>
2009-11-06 14:05:36 +0900

    compatible with Ruby 1.9

--- see.rb
+++ see.rb
@@ -1,18 +1,20 @@
-#! /bin/env ruby
+#!/usr/bin/env ruby
+# coding: utf-8
+
 require 'rubygems'
 require 'httparty'
 require 'pit'
 
 class Twitter
-	include HTTParty
-	base_uri 'twitter.com'
-	config = Pit.get("twitter", :require => {
-					"user_id" => "required id",
-					"password" => "required password"})
-	basic_auth config["user_id"], config["password"]
-	format :xml
+  include HTTParty
+  base_uri 'twitter.com'
+  config = Pit.get("twitter", :require => {
+    "user_id"  => "required id",
+    "password" => "required password"
+  })
+  basic_auth *config.values_at("user_id", "password")
+  format :xml
 end
 
-see = ARGV.map{|arg| "@#{arg}"}.join(" ")
-see << " <〇> <〇>"
+see = ARGV.map {|arg| "@#{arg}" }.join(" ") + " <〇> <〇>"
 Twitter.post('/statuses/update.xml', :query => {:status => see})
